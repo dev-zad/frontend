9dbfb0e922e79e2a5ae4a9830f37853e967dd8c2
zadrach caunca <zadrachcaunca777@gmail.com>
2024-06-08 11:39:15 +0800

    chore: Comment out unused code in dashboard summaries page

--- src/components/custom/Header.tsx
+++ src/components/custom/Header.tsx
@@ -48,7 +48,7 @@ export async function Header({ data }: Readonly<HeaderProps>) {
   return (
     <div className="flex items-center justify-between px-4 py-3 bg-white shadow-md dark:bg-gray-800">
       <Logo text={logoText.text} />
-      {user.ok && <SummaryForm />}
+      {/* {user.ok && <SummaryForm />} */}
       <div className="flex items-center gap-4">
         {user.ok ? (
           <LoggedInUser userData={user.data} />
--- src/components/custom/Search.tsx
+++ src/components/custom/Search.tsx
@@ -1,35 +1,35 @@
-"use client";
-import { usePathname, useRouter, useSearchParams } from "next/navigation";
-import { useDebouncedCallback } from "use-debounce";
-import { Input } from "@/components/ui/input";
+// "use client";
+// import { usePathname, useRouter, useSearchParams } from "next/navigation";
+// import { useDebouncedCallback } from "use-debounce";
+// import { Input } from "@/components/ui/input";
 
-export function Search() {
-  const searchParams = useSearchParams();
-  const { replace } = useRouter();
-  const pathname = usePathname();
+// export function Search() {
+//   const searchParams = useSearchParams();
+//   const { replace } = useRouter();
+//   const pathname = usePathname();
 
-  const handleSearch = useDebouncedCallback((term: string) => {
-    // console.log(`Searching... ${term}`);
-    const params = new URLSearchParams(searchParams);
-    params.set("page", "1");
+//   const handleSearch = useDebouncedCallback((term: string) => {
+//     // console.log(`Searching... ${term}`);
+//     const params = new URLSearchParams(searchParams);
+//     params.set("page", "1");
 
-    if (term) {
-      params.set("query", term);
-    } else {
-      params.delete("query");
-    }
+//     if (term) {
+//       params.set("query", term);
+//     } else {
+//       params.delete("query");
+//     }
 
-    replace(`${pathname}?${params.toString()}`);
-  }, 300);
+//     replace(`${pathname}?${params.toString()}`);
+//   }, 300);
 
-  return (
-    <div>
-      <Input
-        type="text"
-        placeholder="Search"
-        onChange={(e) => handleSearch(e.target.value)}
-        defaultValue={searchParams.get("query")?.toString()}
-      />
-    </div>
-  );
-}
+//   return (
+//     <div>
+//       <Input
+//         type="text"
+//         placeholder="Search"
+//         onChange={(e) => handleSearch(e.target.value)}
+//         defaultValue={searchParams.get("query")?.toString()}
+//       />
+//     </div>
+//   );
+// }
