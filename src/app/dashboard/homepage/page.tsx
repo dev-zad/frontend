3387f1c0bb3bb7710eb9f104789f2ba4d3577706
zadrach caunca <zadrachcaunca777@gmail.com>
2024-06-11 00:33:26 +0800

    Refactor fetchServerData function and update API request handling

--- src/app/dashboard/homepage/page.tsx
+++ src/app/dashboard/homepage/page.tsx
@@ -1,30 +1,44 @@
 "use client";
-
 import LifeButton from '@/components/ui/lgButton';
 import { DataTable } from '@/screens/connects/data-table';
 import ProfileFormContent from '@/screens/connects/modal/ProfileFormContent';
 import React, { useState, useEffect } from 'react';
 import { columns, Connected } from '@/screens/connects/column';
 
-async function fetchServerData(): Promise<Connected[]> {
-    const response = await fetch('/api/messages');
-    if (!response.ok) {
-        throw new Error('Failed to fetch connects');
+async function getData(): Promise<Connected[]> {
+    try {
+        const response = await fetch('/api/messages');
+        if (!response.ok) {
+            throw new Error('Failed to fetch connects');
+        }
+        const responseData = await response.json();
+        const data = responseData.map((item: any) => item.attributes); // Extract data from "attributes" property
+        return data;
+    } catch (error) {
+        console.error('Error fetching connects:', error);
+        return [];
     }
-    const responseData = await response.json();
-    return responseData.map((item: any) => item.attributes);
 }
 
-export default function HomeRoute() {
+
+
+export default async function HomeRoute() {
     const [data, setData] = useState<Connected[]>([]);
     const [loading, setLoading] = useState<boolean>(true);
     const [error, setError] = useState<string | null>(null);
 
     useEffect(() => {
-        fetchServerData()
-            .then(setData)
-            .catch(err => setError(err.message))
-            .finally(() => setLoading(false));
+        async function fetchData() {
+            try {
+                const connects = await getData();
+                setData(connects);
+            } catch (error: any) {
+                setError(error.message);
+            } finally {
+                setLoading(false);
+            }
+        }
+        fetchData();
     }, []);
 
     if (loading) return <div>Loading...</div>;
