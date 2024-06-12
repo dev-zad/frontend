ddd8ed248eba0afc71a1fe90a12f88053dd1ff60
zadrach caunca <zadrachcaunca777@gmail.com>
2024-06-12 14:19:40 +0800

    Refactor submitForm API handler to use nodemailer for email sending

--- src/pages/api/submitForm.ts
+++ src/pages/api/submitForm.ts
@@ -3,33 +3,27 @@ import nodemailer from 'nodemailer';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
-        // Handle form submission logic here
         const { email, fullName, tribe, leader } = req.body;
         console.log('Form data:', { email, fullName, tribe, leader });
 
-        // Create a Nodemailer transporter
         let transporter = nodemailer.createTransport({
-            service: 'gmail', // replace with your email service
+            service: 'gmail',
             auth: {
                 user: process.env.EMAIL_USERNAME,
                 pass: process.env.EMAIL_PASSWORD
             }
         });
 
-        // Set up email data
         let mailOptions = {
-            from: process.env.EMAIL_USERNAME, // sender address
-            to: email, // list of receivers
-            // Subject line with form details
+            from: process.env.EMAIL_USERNAME,
+            to: email,
             subject: `Form Submission - ${fullName}, ${tribe}, ${leader}`,
-            // Text body with form details
             text: `Form submitted successfully by ${fullName}.
                     Email: ${email}
                     Tribe: ${tribe}
                     Leader: ${leader}`
         };
 
-        // Send email
         transporter.sendMail(mailOptions, (error, info) => {
             if (error) {
                 console.log(error);
